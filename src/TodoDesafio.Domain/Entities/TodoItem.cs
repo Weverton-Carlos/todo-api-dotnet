50c43fa52f1a335e7f9d424d1ef5b3c8efe435ce
Weverton <weverton_carlos@outlook.com>
2026-04-24 11:52:11 -0300

    refactor: add BaseEntity

--- src/TodoDesafio.Domain/Entities/TodoItem.cs
+++ src/TodoDesafio.Domain/Entities/TodoItem.cs
@@ -2,14 +2,13 @@ using TodoDesafio.Domain.Enums;
 
 namespace TodoDesafio.Domain.Entities;
 
-public class TodoItem
+public class TodoItem : BaseEntity
 {
-    public int Id { get; set; }
     public string Title { get; set; } = string.Empty;
+
     public string? Description { get; set; }
+
     public Status Status { get; set; } = Status.Pending;
-    public DateTime CreatedAt { get; set; }
-    public DateTime? UpdatedAt { get; set; }
+
     public DateTime DueDate { get; set; }
-    public bool IsDeleted { get; set; }
 }
